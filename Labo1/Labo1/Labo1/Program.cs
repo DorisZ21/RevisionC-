4fd5f1ecf098f09d3a85d434eea4443c32fb3811
DorisZ21 <etu54288@henallux.be>
2025-01-14 16:29:56 +0100

    premier commit

---
+++ Labo1/Labo1/Labo1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace labo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world !");
+        }
+    }
+}
+
